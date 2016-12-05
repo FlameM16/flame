cef3e2c7439bdda9b2f24ee775e5f2be33e94914
FlameM16 <flamem36@gmail.com>
2016-12-05 17:17:54 +0200

    Update TimeZona.java

--- TimeZona.java
+++ TimeZona.java
@@ -3,18 +3,16 @@ import java.util.Date;
 import java.util.TimeZone;
 import java.util.Scanner;
 
-
 public class TimeZona {
-
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter TimeZone:");
-		String s = "GMT"+scan.nextLine();
-		System.out.println("Time in Ukraine:");
-		SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MMMyyyy \n  HH:mm 'GMT'Z \n  K:m a ");
+	Scanner scan = new Scanner(System.in);
+	System.out.println("Enter TimeZone:");
+	String s = "GMT"+scan.nextLine();
+	System.out.println("Time in Ukraine:");
+	SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MMMyyyy \n  HH:mm 'GMT'Z \n  K:m a ");
         System.out.println("date: " + dateFormat.format( new Date() ) );
         SimpleDateFormat dateFormat1 = new SimpleDateFormat("MM/dd/yyyy \n  HH:mm 'GMT'Z \n  K:m a ");
         dateFormat1.setTimeZone(TimeZone.getTimeZone(s));
         System.out.println("date: " + dateFormat1.format( new Date() ) );
-	}
+}
 }
